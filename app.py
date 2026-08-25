9ae3d4ff3bdbff0ee1f6efea60d60bc359f8289e
Doma Saketh <sakethdoma61@gmail.com>
2026-08-25 09:30:33 +0530

    Initialize database on deployment

--- app.py
+++ app.py
@@ -178,6 +178,7 @@ def attendance():
                                   JOIN sessions s ON s.id=a.session_id ORDER BY a.id DESC""").fetchall(); c.close()
     return render_template("attendance.html",rows=rows)
 
+init_db()
+
 if __name__=="__main__":
-    init_db()
-    app.run(debug=True,host="127.0.0.1",port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000)
